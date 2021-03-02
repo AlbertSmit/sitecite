ea106ad364feb808c0779d0fabd445166a1f243b
Albert Smit <talkto@albertsm.it>
2021-03-02 17:44:01 +0100

    v1.0.1 release ;-)

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const fetch = require("isomorphic-fetch");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { promises: fs } = require("fs");
+const github = require("@actions/github");
+
+const getPullRequestNumber = (ref) => {
+  core.debug(`Parsing ref: ${ref}`);
+  // This assumes that the ref is in the form of `refs/pull/:prNumber/merge`
+  const prNumber = ref.replace(/refs\/pull\/(\d+)\/merge/, "$1");
+  return parseInt(prNumber, 10);
+};
 
 async function run() {
   try {
@@ -41,6 +49,19 @@ async function run() {
       const dom = await new JSDOM(text);
       core.info(dom.window.document.querySelector("h1").textContent);
     })();
+
+    /**
+     * Comment on given PR.
+     */
+    const context = github.context;
+    const myToken = core.getInput("token");
+    const octokit = github.getOctokit(myToken);
+    octokit.pulls.createReviewComment({
+      ...context.repo,
+      pull_number:
+        github.context.issue.number || getPullRequestNumber(context.ref),
+      body: "Test!",
+    });
 
     core.setOutput("report", "Nothing yet. Just testing.");
   } catch (error) {
