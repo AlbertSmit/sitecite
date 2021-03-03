c462b94b57856f7565e883f33708bf5a5d7720f2
Albert Smit <talkto@albertsm.it>
2021-03-03 13:01:44 +0100

    Fail on not found

--- src/emoji.ts
+++ src/emoji.ts
@@ -1,6 +1,7 @@
 /**
  * Emoji
  */
+export const smile: string = ":slightly_smiling_face:";
 export const yay: string = ":white_check_mark:";
 export const nay: string = ":no_entry_sign:";
 export const script: string = ":scroll:";
