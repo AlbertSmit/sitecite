b6954d1ee36bbd0e8d9b1cc95049de3e67c8575f
Albert Smit <talkto@albertsm.it>
2021-03-04 19:04:41 +0100

    Add test

--- tests/mock/output.ts
+++ tests/mock/output.ts
@@ -1,6 +1,14 @@
 import { CiteResult } from "../../dist/comment";
 
-export const citations: CiteResult[] = [
+export const singleCitation: CiteResult[] = [
+  {
+    source: "I have a dream!",
+    found: false,
+    cite: "https://www.america.com",
+  },
+];
+
+export const multiCitations: CiteResult[] = [
   {
     source: "Ik moest kloppen want de bel deed het neit",
     found: false,
